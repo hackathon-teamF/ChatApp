be9a3903d1c9abda386c77f3a35dd49cf1b5390c
mkk91026 <mikiko@MIKIKOnoMacBook-Air.local>
2022-11-09 15:18:29 +0900

    util done

---
+++ util/DB.py
@@ -0,0 +1,16 @@
+import pymysql
+
+class DB:
+    def getConnection():
+        try:
+            conn = pymysql.connect(
+            host="localhost",
+            db="cahtapp",
+            user="testuser",
+            charset="utf-8",
+            cursorclass=pymysql.cursors.DiscCursor
+        )
+            return conn
+        except (ConnectionError):
+            print("コネクションエラーです")
+            conn.close()
